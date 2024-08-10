5dec2ac3c7454ca40d8ea39a0194d00cd3ca4f66
Khurram Ilyas <khuram.ilyass@gmail.com>
2024-08-10 22:17:23 +0500

    Pagination
    Per Page option not provided in UI to keep things simple

--- api/controllers/users.ts
+++ api/controllers/users.ts
@@ -24,7 +24,28 @@ export const getUserProfile = jsonWrap(async (req: Request) => {
 });
 
 export const getIndex = async (req: Request, res: any) => {
-  const scores = await ScoreModel.find({}); //[ { title: 'ab'}, { title: 'cd'}] //
-  
-  res.render('index', { scores });
+
+  // Get page and perPage from query parameters, with default values
+  let page = parseInt(req.query.page as string, 10) || 1; // Default to page 1 if not provided
+  const perPage = parseInt(req.query.perPage as string, 10) || 198; // Default to 15 if not provided
+
+  // Get total number of records
+  const totalRecords = await ScoreModel.countDocuments({}).exec();
+
+  // Calculate total number of pages
+  const totalPages = Math.ceil(totalRecords / perPage);
+
+  page = page < totalPages ? page : totalPages;
+
+  // Calculate the number of records to skip
+  const skip = (page - 1) * perPage;
+
+  // Query the database with pagination
+  const scores = await ScoreModel.find({})
+    .skip(skip)
+    .limit(perPage)
+    .exec();
+
+  // Render the results
+  res.render('index', { scores, page, perPage, totalPages });
 }
